155c4ee07fcdb1fd59dc5398a5ae56e8401e3c48
unknown <sushantdhungana29@gmail.com>
2023-02-15 12:50:22 +0545

    topbar and links

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import './topbar.css'
 import Logo from "../images/logo.png"
+import { BsFillCalendarCheckFill } from 'react-icons/bs'
 const TopBar = () => {
-  return (
-    <div>
-      <div className="container">
-        <div className="row">
-            <div className="col-md-2">
-                <img src={Logo} alt="logo" className='logo_top'/>
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <img src={Logo} alt="logo" className='logo_top' />
+                    </div>
+                    <div className="col-md-6 top_second">
+                        <div className="book_appointment">
+                        <BsFillCalendarCheckFill size={30} color="#dc3545ba"/>
+                        <a href='/'>Book Appointment</a>
+                        </div>
+                        <div className="event_registration">
+                        <BsFillCalendarCheckFill size={30} color="#dc3545ba"/>
+                        <a href='/'>Event Registration</a>
+                        </div>
+                    </div>
+                    <div className="col-md-3 top_third">
+                            <button className="apply_button">Apply Now</button>
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default TopBar
