df186001c604d9543353fa6981a3680a3f504cab
unknown <sushantdhungana29@gmail.com>
2023-02-16 16:56:36 +0545

    navbar upside banner

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,57 +1,141 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import banner from "../images/banner.png"
 import "../styles/carousel.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const CarouselMain = () => {
     var settings = {
         dots: true,
         infinite: true,
-        arrows:true,
+        arrows: true,
         speed: 500,
+        autoplay: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         responsive: [
             {
-              breakpoint: 1000,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true,
-                vertical: false,
-                arrows: false,
-              },
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    vertical: false,
+                    arrows: false,
+                },
             },
-          ],
-      };
+        ],
+    };
 
-  return (
-  <div className='carousel_main'>    <Slider {...settings}>
-  <div>
-    <h3><img src={banner} alt="banner"/></h3>
-  </div>
-  <div>
-  <h3><img src={banner} alt="banner"/></h3>
-  </div>
-  <div>
-  <h3><img src={banner} alt="banner"/></h3>
-  </div>
-  <div>
-  <h3><img src={banner} alt="banner"/></h3>
-  </div>
-  <div>
-  <h3><img src={banner} alt="banner"/></h3>
-  </div>
-  <div>
-  <h3><img src={banner} alt="banner"/></h3>
-  </div>
-</Slider>
-</div>
-  )
+    return (
+        <div className='carousel_main'>    <Slider {...settings}>
+            <div>
+                <h3><img src={banner} alt="banner" /></h3>
+            </div>
+            <div>
+                <h3><img src={banner} alt="banner" /></h3>
+            </div>
+            <div>
+                <h3><img src={banner} alt="banner" /></h3>
+            </div>
+            <div>
+                <h3><img src={banner} alt="banner" /></h3>
+            </div>
+            <div>
+                <h3><img src={banner} alt="banner" /></h3>
+            </div>
+            <div>
+                <h3><img src={banner} alt="banner" /></h3>
+            </div>
+        </Slider>
+            <Navbar collapseOnSelect expand="lg">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="m-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <NavDropdown title="Services" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Study Abroad" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="About" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Preparation" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Courses" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#gallery">Gallery</Nav.Link>
+                            <NavDropdown title="Information" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
 }
 
 export default CarouselMain
