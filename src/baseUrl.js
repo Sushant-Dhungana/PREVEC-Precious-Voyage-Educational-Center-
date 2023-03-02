4fbdb71052fbd0c3914a3913e4356559cd169d71
sushant dhungana <54764576+Sushant-Dhungana@users.noreply.github.com>
2023-03-02 14:49:05 +0545

    index

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -2,9 +2,6 @@
 import Axios from 'axios';
 const axiosBaseURL = Axios.create({
     baseURL: process.env.baseUrl || "http://192.168.1.12:8000",
-    headers: {
-        "Content-Type": "multipart/form-data",
-      },
 
 });
 export default axiosBaseURL;
--- src/components/Callback.jsx
+++ src/components/Callback.jsx
@@ -1,48 +1,48 @@
-import React,{useState} from 'react'
-import "../styles/callback.css"
-import mobile from "../images/mobile.png"
-import axiosBaseURL from "../baseUrl";
+// import React,{useState} from 'react'
+// import "../styles/callback.css"
+// import mobile from "../images/mobile.png"
+// import axiosBaseURL from "../baseUrl";
+// import axios from 'axios';
 
-const Callback = () => {
+// const Callback = () => {
 
-    const [phone_number, setNumber] = useState("")
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axiosBaseURL.post('/api/get-call-back', phone_number)
-      .then((response) => {
-        setNumber(response.data)
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    }
-    const handleChange = (e) => {
-        setNumber(e.target.value)
-      }
-  return (
-    <div>
-        <div className="callback_main">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-3">
-                        <div className="callback_image">
-                            <img src={mobile} alt="mobile" />
-                        </div>
-                    </div>
-                    <div className="col-md-9">
-                        <div className="callback_content">
-                            <h2>Get a Callback From Our Experts</h2>
-                            <form className='callback_form' onSubmit={handleSubmit}>
-                                <input type="tel" placeholder="Enter Your Number" name="phone_number" value={phone_number.name}  onChange={handleChange} required/>
-                                <button type='submit'>Request</button>
-                            </form>
-                            </div>
-                    </div>
-                </div>
-            </div>
-            </div>
-    </div>
-  )
-}
+//     const [phone_number, setNumber] = useState("")
 
-export default Callback
+//     var bodyFormData = new FormData();
+//     bodyFormData.append('phone_number', phone_number);
+
+//     const handleSubmit = (e) => {
+//         axios.post("http://192.168.1.12:8000/api/get-call-back", bodyFormData, { headers: { 'Content-Type': 'multipart/form-data' } })
+//         .then(console.log)
+//         .catch(console.error);
+//      e.preventDefault();
+//     }
+//     console.log(phone_number)
+
+//   return (
+//     <div>
+//         <div className="callback_main">
+//             <div className="container">
+//                 <div className="row">
+//                     <div className="col-md-3">
+//                         <div className="callback_image">
+//                             <img src={mobile} alt="mobile" />
+//                         </div>
+//                     </div>
+//                     <div className="col-md-9">
+//                         <div className="callback_content">
+//                             <h2>Get a Callback From Our Experts</h2>
+//                             <form className='callback_form' onSubmit={handleSubmit}>
+//                                 <input type="number" placeholder="Enter Your Number" name="phone_number" value={phone_number}  onChange={(e) => setNumber(e.target.value)} required maxLength={10}/>
+//                                 <button type='submit'>Request</button>
+//                             </form>
+//                             </div>
+//                     </div>
+//                 </div>
+//             </div>
+//             </div>
+//     </div>
+//   )
+// }
+
+// export default Callback
